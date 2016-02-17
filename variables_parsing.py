a6dd4c512d9ba9632cee8a256c7433e451e00520
Jamie Kettleborough <jamie.kettleborough@metoffice.gov.uk>
2016-02-17 16:06:48 +0000

    updates and tidy

--- variables_parsing.py
+++ variables_parsing.py
@@ -6,6 +6,10 @@ import csv
 import string
 from mip_parser import parseMipTable as mip_table_read
 
+_SEP='_'
+def mip_id(table, section):
+    return _SEP.join((table, section))
+
 class VariableEntry(object):
     """
     Represents an entry in a xxx_variables file.
@@ -13,55 +17,48 @@ class VariableEntry(object):
     Example
     ------
 
-    >>> entry = VariableEntry('apm_variables','od550aer', dict(lbproc=128,
+    >>> entry = VariableEntry('apm','od550aer', dict(lbproc=128,
     ...          lbuser5=3, miptable='CMIP5_aero', outputs_per_file='10'))
     >>> entry.stream, entry.mip_id, entry.published, entry.selection
     ('apm', 'CMIP5_aero_od550aer', 'CMIP5 (aero, od550aer)', 'lbproc: 128, lbuser5: 3')
 
-    >>> entry = VariableEntry('apm_variables', 'sbl_1', dict(miptable='CMIP5_Lmon'))
+    >>> entry = VariableEntry('apm', 'sbl_1', dict(miptable='CMIP5_Lmon'))
     >>> entry.mip_id
     'CMIP5_Lmon_sbl'
     """
     
     _SELECTORS = ('lbproc', 'lbuser5', 'blev')
-    _SEP = '_'
     
-    def __init__(self, fname, section, options):
-        self.stream = fname.split(self._SEP)[0]
-        self._section = section.split(self._SEP)[0]
-        doptions = dict(options)
-        self.mip_id = doptions.pop('miptable') + self._SEP + self._section
-        self._published(doptions)
-        if 'outputs_per_file' in doptions:
-            doptions.pop('outputs_per_file')
-        self.stash_mapping = self.published
-        self._options = doptions
-        
+    def __init__(self, fname, section, attdict):
+        clean_section = section.split(_SEP)[0] # remove any _1's or _2's
+        self.mip_id = mip_id(attdict['miptable'], clean_section)
+        self.published = attdict.setdefault('mapping_id', "{} ({}, {})".format(*self.mip_id.split(_SEP)))
+        self._attdict = attdict
+
     def __getattr__(self, attname):
-        return self._options[attname]
+        if attname not in self._attdict:
+            raise AttributeError('Attribute not found: {}'.format(attname))
+        else:
+            return self._attdict[attname]
     
     @property
     def selection(self):
-        selectors = {k:v for k, v in self._options.iteritems() if k in self._SELECTORS}
+        selectors = {k:v for k, v in self._attdict.iteritems() if k in self._SELECTORS}
         return ', '.join(['{}: {}'.format(k, v) for k, v in selectors.iteritems()])
     
-    def _published(self, options):
-        if 'mapping_id' in options:
-            result = options.pop('mapping_id')
-        else:
-            result = "{} ({}, {})".format(*self.mip_id.split('_'))
-        self.published = result
-            
 
 def read_variables_file(fname):
     """Read the xxx_variables file returning a list of entries."""
+    
     with open(fname, 'r') as fi:
         parser = ConfigParser.SafeConfigParser()
         parser.readfp(fi)
-    stream = os.path.basename(fname)
-    return [VariableEntry(stream, section, parser.items(section)) for section in parser.sections()]
+    stream = os.path.basename(fname).split(_SEP)[0]
+    return [VariableEntry(stream, section, dict(parser.items(section))) for section in parser.sections()]
 
 def read_variables_dir(dname):
+    """Read a directory of xxx_variables files returning all entries."""
+    
     fnames = [fname for fname in os.listdir(dname) if 'variables' in fname]
     variables = list()
     for fname in fnames:
@@ -70,15 +67,16 @@ def read_variables_dir(dname):
 
 
 class MappingExpression(object):
+    """Class representing the entries in the stash mapping table."""
     
-    _T = string.maketrans(' ', '_')
+    _TRANS = string.maketrans(' ', _SEP)
     
     def __init__(self, adict):
         adict.pop('')
         self._attdict = self._strip(adict)
         
     def _strip(self, adict):
-        return {k[2:-2].translate(self._T).lower(): v.strip() for k,v in adict.iteritems()}
+        return {k[2:-2].translate(self._TRANS).lower(): v.strip() for k,v in adict.iteritems()}
 
     def __getattr__(self, attname):
         if attname not in self._attdict:
@@ -90,11 +88,12 @@ class MappingExpression(object):
         """Return True if this expression is appropriate for version."""
         return eval('{} {}'.format(version, self.um_version))
 
-def read_stash_mapping(mi, version):
-    mapping_reader = csv.DictReader(mi, delimiter = '|')
-    expressions = (MappingExpression(entry) for entry in mapping_reader)
-
-    expressions = [expression for expression in expressions if expression.for_version(version)]
+def read_stash_mapping(ifile, version):
+    with open(ifile, 'r') as mi:
+        mapping_reader = csv.DictReader(mi, delimiter = '|')
+        expressions = (MappingExpression(entry) for entry in mapping_reader)
+        expressions = filter(lambda e: e.for_version(version), expressions)
+        
     return expressions
 
 
@@ -129,8 +128,8 @@ class MipTableVariableEntry(object):
         self.entry = entry
         self.table = table_name.split(self._SEP)[1]
         self.table_name = table_name
-        self.mip_id = self._SEP.join((table_name, entry))
-        self.short_mip_id = self._SEP.join((self.table, self.entry))
+        self.mip_id = mip_id(table_name, entry)
+        self.short_mip_id = mip_id(self.table, self.entry)
         self.variable = NoExpression()
        
         for attname in ('positive', 'cell_methods'):
@@ -171,11 +170,15 @@ class MipCsvVariableEntry(object):
     def __init__(self, adict):
         self.entry = adict['cmor_label']
         self.table = adict['miptable']
-        if self.table == 'cfsites':
-            self.table = 'cfSites'
-        self.short_mip_id = '_'.join((self.table, self.entry))
+        self._correct_atts()
+        self.short_mip_id = mip_id(self.table, self.entry)
         self.attdict = adict
         self._tidy_dict()
+
+        
+    def _correct_atts(self):
+        if self.table == 'cfsites':  # take into account case issues
+            self.table = 'cfSites'
 
     def _tidy_dict(self):
         
@@ -201,17 +204,21 @@ def read_mip_dir(dname, project):
     return requests
 
 def read_mip_table(fname):
+    """Return a list of MIP requested variables for a miptable."""
+    
     table = mip_table_read(fname)
     tname = os.path.basename(fname)
     requests = (MipTableVariableEntry(tname, entryname, entry)
                   for entryname, entry in table['vars'].iteritems())
     return filter(lambda r: r.is_variable, requests)
 
-def read_cmip6_csv(fi):
-    reader = csv.DictReader(fi)
-    return [MipCsvVariableEntry(record) for record in reader]
+def read_cmip6_csv(mip_csv):
+    """Return a list of entries from the CMIP6 request spreadsheet."""
+    with open(mip_csv, 'r') as mi:
+        reader = csv.DictReader(mi)
+        return [MipCsvVariableEntry(record) for record in reader]
 
-def expression_for_variables(variables, expressions):
+def add_expression_to_variables(variables, expressions):
     """Matches an expression to a variable using the published attribute."""
         
     for variable in variables:
@@ -232,7 +239,14 @@ def variable_for_request(requests, variables):
                 request.variable = variable
 
 def known_for_required(recs1, requests):
-       for cmip6 in recs1:
+    """
+    Adds to previous know expression information to the new requests.
+
+    Matches a previous MIP diagnostics  with a new MIP diagnostic
+    using the short_mip_id like Amon_tas.
+    """
+    
+    for cmip6 in recs1:
         for cmip5 in requests:
             if cmip6.short_mip_id == cmip5.short_mip_id:
                 cmip6.attdict['Variable_mapping'] = cmip5.variable.stash_mapping
@@ -242,25 +256,24 @@ def known_for_required(recs1, requests):
                 cmip6.attdict['Model_positive'] = cmip5.variable.positive
                 cmip6.attdict['Model_units'] = cmip5.variable.units
                 
+def known_mappings(vdir, tdir, mfile, version):
+    """
+    Return a list of known mappings for a model version.
 
-def fill_cmip6(mip_csv, mfile, vdir, tdir, ofile):
-    with open(mfile, 'r') as mi:
-        expressions = read_stash_mapping(mi, 6.6)
+    The mappings are inferred from the XXX_variables files,
+    the MIP tables, the stash mapping file. 
+    """
+    
+    expressions = read_stash_mapping(mfile, version)
     variables = read_variables_dir(vdir)
-    expression_for_variables(variables, expressions)
+    add_expression_to_variables(variables, expressions)
     
     requests = read_mip_dir(tdir, 'CMIP5') # improve this CMIP5 hard coded
     variable_for_request(requests, variables)
 
-    requests = filter(lambda r: r.has_mapping, requests) # only CMIP5 with requests
+    return filter(lambda r: r.has_mapping, requests) # only CMIP5 with requests
 
-    with open(mip_csv, 'r') as mi:
-        recs1 = read_cmip6_csv(mi)
-
-    known_for_required(recs1, requests)    
-    write_csv2(ofile, recs1)
-
-def write_csv2(ofile, recs1):
+def write_csv(ofile, recs1):
     with open(ofile, 'wb') as mi:
         fieldnames = "cmor_label,title,miptable,cf_std_name,description,cell_methods,dimension,units,positive,realm,priority,requesting_mips,UKESM_component,Owner,Variable_mapping,PP_constraint,Model_units,Model_positive,Stream,Plan,Ticket,Comment,Notes".split(',')
 
@@ -268,16 +281,21 @@ def write_csv2(ofile, recs1):
         writer.writeheader()
         for rec in recs1:
             writer.writerow(rec.attdict)
-                
-def main(mfile, vdir, tdir, ofile):
-    fill_cmip6('input/CMIP6_data_req_20151126.csv', mfile, vdir, tdir, 'out2.csv')
+
+def fill_cmip6(mip_csv, mfile, vdir, tdir, ofile):
     
+    requests = known_mappings(vdir, tdir, mfile, 6.6)
+    cmip6 = read_cmip6_csv(mip_csv)
+
+    known_for_required(cmip6, requests)    
+    write_csv(ofile, cmip6)
+
 if __name__ == '__main__':
 
     bdir = '/project/ipcc/ar5/etc'
-    ofile = 'out.csv'
+    ofile = 'out2.csv'
     tdir = os.path.join(bdir, 'mip_tables/CMIP5/20130717')
-    vdir = '/project/cfmip/ar5_proc_CMIP5_MOHC/trunk/HadGEM2-ES'
     mfile = os.path.join(bdir, 'mapping_tables/stash_mappings.txt')
-        
-    main(mfile, vdir, tdir, ofile)
+    vdir = '/project/cfmip/ar5_proc_CMIP5_MOHC/trunk/HadGEM2-ES'
+    cmip6_requests = 'input/CMIP6_data_req_20151126.csv'
+    fill_cmip6(cmip6_requests, mfile, vdir, tdir, ofile)
